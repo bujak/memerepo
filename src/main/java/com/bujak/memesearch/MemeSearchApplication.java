255dccc0e2733ee9f1b27cb9de2911f89924d67c
Tomasz Bujakowski <bujakowski@gmail.com>
2019-06-14 18:18:23 +0200

    Initial commit

---
+++ src/main/java/com/bujak/memesearch/MemeSearchApplication.java
@@ -0,0 +1,13 @@
+package com.bujak.memesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemeSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemeSearchApplication.class, args);
+    }
+
+}
