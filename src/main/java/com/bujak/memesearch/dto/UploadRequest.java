9eabab621fed709f627bedcbe6069612efb186a4
Tomasz Bujakowski <bujakowski@gmail.com>
2019-06-16 11:06:16 +0200

    Add file field in UploadRequest

--- src/main/java/com/bujak/memesearch/dto/UploadRequest.java
+++ src/main/java/com/bujak/memesearch/dto/UploadRequest.java
@@ -1,6 +1,7 @@
 package com.bujak.memesearch.dto;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Set;
 
@@ -9,5 +10,5 @@ public class UploadRequest {
 
     private String name;
     private Set<String> keywords;
-
+    private MultipartFile file;
 }
